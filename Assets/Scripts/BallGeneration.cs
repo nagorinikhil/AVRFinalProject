a0253c774c687e1b54eeebf4fa050c9776040fc9
Nikhil Nagori <nagorinikhil@gmail.com>
2017-05-12 11:00:56 -0400

    ITCS 6125 AVR Project final commit 5_2_2017

---
+++ Assets/Scripts/BallGeneration.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallGeneration : MonoBehaviour {
+
+    public GameObject ballPreFab;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void GenerateBall()
+    {
+        GameObject instance = Instantiate(ballPreFab);
+        instance.transform.position = new Vector3(0, -0.111f, 20.71f);
+    }
+}
---
+++ Assets/Scripts/BallTouchTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallTouchTrigger : MonoBehaviour {
+
+    public GameObject ballPreFab;
+    public float ballSpeed = 5.0f;
+
+
+    void OnTriggerEnter(Collider collider)
+    {
+        GameObject instance = Instantiate(ballPreFab);
+        instance.transform.position = new Vector3(3.5f, 3, 24.5f);
+        //Rigidbody rb = instance.GetComponent<Rigidbody>();
+        //Camera camera = GetComponentInChildren<Camera>();
+        //rb.velocity = camera.transform.rotation * Vector3.forward * ballSpeed;
+    }
+}
